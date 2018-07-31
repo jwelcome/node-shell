068487d39c10e5e67a4afeee6c40b4b348e16ac2
Juliet Welcome <juliet.welcome@gmail.com>
2018-07-31 11:28:36 -0500

    ls, cat

--- bash.js
+++ bash.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const pwd = require('./pwd');
+const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') pwd();
+
+  const parsedCmd = cmd.split(' ');
+
+  if (parsedCmd[0] === 'pwd') pwd();
+  if (parsedCmd[0] === 'ls') ls();
+  if (parsedCmd[0] === 'cat') cat(parsedCmd[1]);
 });
--- ls.js
+++ ls.js
@@ -1,1 +1,12 @@
-module.exports = function() {};
+const fs = require('fs');
+
+module.exports = function() {
+  fs.readdir('./', 'utf8', (err, files) => {
+    if (err) {
+      throw err
+    } else {
+      process.stdout.write(files.join('\n'))
+      process.stdout.write("prompt > ");
+    }
+  })
+};
